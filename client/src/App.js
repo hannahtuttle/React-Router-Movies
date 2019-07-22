348bfda4355b82c142cc96df063575f179e8f6ba
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2019-07-22 13:23:37 -0700

    added path from Movie list, working on adding path for movie with :id

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-
+import { Route } from 'react-router-dom'
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList.js'
+import Movie from './Movies/Movie.js'
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -12,7 +14,8 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route path='/' exact component={MovieList}/>
+      <Route path='/movies/' component={Movie}/>
     </div>
   );
 };
